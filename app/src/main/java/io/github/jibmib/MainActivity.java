4bdb9fb7ae7dc609e7dad81e6c68ae37a86c40ed
Farhad Faghihi <farhadfaghihi@outlook.com>
2017-08-08 12:27:16 +0430

    upgraded to version 2.0.3

--- app/src/main/java/io/github/jibmib/MainActivity.java
+++ app/src/main/java/io/github/jibmib/MainActivity.java
@@ -1,17 +1,13 @@
 package io.github.jibmib;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
-import static java.security.AccessController.getContext;
+import ir.jibmib.pidgets.utils.MonetaryTextWatcher;
+import ir.jibmib.pidgets.widget.ParsiButton;
+import ir.jibmib.pidgets.widget.ParsiEditText;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +15,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ParsiEditText editText = (ParsiEditText)findViewById(R.id.etVerifyCode) ;
+
+        editText.addTextChangedListener(new MonetaryTextWatcher(editText));
+
+        ParsiButton button = (ParsiButton)findViewById(R.id.btn) ;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,editText.getText().toString(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
