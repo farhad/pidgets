2b6ec2d18cdaa3c7a34ce15669e547ecaa7422fa
Farhad <ifarhadfaghihi@gmail.com>
2018-03-31 00:55:53 +0430

    fixes and enhances in ParsiTextView

--- pidgets/src/main/java/io/github/farhad/widget/ParsiTextView.java
+++ pidgets/src/main/java/io/github/farhad/widget/ParsiTextView.java
@@ -12,101 +12,75 @@ import io.github.farhad.utils.Utils;
 
 public class ParsiTextView extends AppCompatTextView {
 
-    private boolean shouldReplaceWithParsiDigits;
-    private FontType fontType;
+    private boolean useParsiDigits;
+    private FontType typefaceStyle;
 
     public ParsiTextView(Context context) {
         super(context);
 
-        init(context);
-    }
+        TypedArray typedArray = context.obtainStyledAttributes(R.styleable.ParsiTextView) ;
 
-    public ParsiTextView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+        initialize(context,typedArray);
 
-        init(context, attrs);
+        typedArray.recycle();
     }
 
     public ParsiTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        init(context, attrs);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.ParsiTextView) ;
+
+        initialize(context,typedArray);
+
+        typedArray.recycle();
+    }
+
+    public ParsiTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.ParsiTextView,defStyle,defStyle) ;
+
+        initialize(context,typedArray);
+
+        typedArray.recycle();
+    }
+
+    private void initialize(Context context,TypedArray typedArray) {
+
+        if (!isInEditMode()) {
+
+            useParsiDigits = typedArray.getBoolean(R.styleable.ParsiTextView_useParsiDigits, true);
+            typefaceStyle = FontType.getType(typedArray.getInt(R.styleable.ParsiTextView_typefaceStyle, 0));
+
+            typedArray.recycle();
+
+            setTypeface(ParsiTypeface.getInstance().getMatchingTypeface(typefaceStyle));
+        }
     }
 
     @Override
     public void setText(CharSequence text, BufferType type) {
 
-        if(!isInEditMode()){
+        if (useParsiDigits && Utils.containsDigits(text.toString()))
+            super.setText(ParsiUtils.replaceWithParsiDigits(text.toString()), type);
 
-            if (shouldReplaceWithParsiDigits && Utils.containsDigits(text.toString())) {
-
-                super.setText(ParsiUtils.replaceWithParsiDigits(text.toString()), type);
-            } else {
-
-                super.setText(text, type);
-            }
-        }
-
-        else {
-
-            super.setText(text,type);
-        }
-
-        requestLayout();
+        else
+            super.setText(text, type);
     }
 
-    private void init(Context context) {
-
-        if(!isInEditMode()){
-
-            TypedArray typedArray = context.obtainStyledAttributes(R.styleable.ParsiTextView);
-
-            shouldReplaceWithParsiDigits = typedArray.getBoolean(R.styleable.ParsiTextView_useParsiDigits,true);
-            fontType = FontType.getType(typedArray.getInt(R.styleable.ParsiTextView_typefaceStyle, 0));
-
-            typedArray.recycle();
-
-            setTypeface(ParsiTypeface.getInstance().getMatchingTypeface(fontType));
-        }
-
-        requestLayout();
+    public boolean useParsiDigits() {
+        return useParsiDigits;
     }
 
-    private void init(Context context, AttributeSet attributeSet) {
-
-        if(!isInEditMode()){
-
-            TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.ParsiTextView, 0, 0);
-
-            shouldReplaceWithParsiDigits = typedArray.getBoolean(R.styleable.ParsiTextView_useParsiDigits, true);
-            fontType = FontType.getType(typedArray.getInt(R.styleable.ParsiTextView_typefaceStyle, 0));
-
-            typedArray.recycle();
-
-            setTypeface(ParsiTypeface.getInstance().getMatchingTypeface(fontType));
-        }
-
-        requestLayout();
+    public void setUseParsiDigits(boolean useParsiDigits) {
+        this.useParsiDigits = useParsiDigits;
     }
 
-
-    public boolean shouldReplaceWithParsiDigits() {
-        return shouldReplaceWithParsiDigits;
+    public FontType getTypefaceStyle() {
+        return typefaceStyle;
     }
 
-    public void setShouldReplaceWithParsiDigits(boolean shouldReplaceWithParsiDigits) {
-        this.shouldReplaceWithParsiDigits = shouldReplaceWithParsiDigits;
-
-        requestLayout();
-    }
-
-    public FontType getFontType() {
-        return fontType;
-    }
-
-    public void setFontType(FontType fontType) {
-        this.fontType = fontType;
-
-        requestLayout();
+    public void setTypefaceStyle(FontType typefaceStyle) {
+        this.typefaceStyle = typefaceStyle;
     }
 }
