80e972320a2787a44d1f554219fed5b1093c1a1a
Farhad <ifarhadfaghihi@gmail.com>
2018-04-26 12:29:02 +0430

    updated the demo app module for bankcardtextwatcher

--- app/src/main/java/ir/farhadfaghihi/MainActivity.java
+++ app/src/main/java/ir/farhadfaghihi/MainActivity.java
@@ -7,14 +7,17 @@ import android.view.KeyEvent;
 import android.view.View;
 
 import io.github.farhad.typeface.FontType;
+import io.github.farhad.utils.PidgetUtils;
 import io.github.farhad.utils.textwatcher.BankCardTextWatcher;
 import io.github.farhad.widget.ParsiButton;
 import io.github.farhad.widget.ParsiEditText;
+import io.github.farhad.widget.ParsiTextView;
 
 public class MainActivity extends Activity {
 
-    AppCompatEditText editTextBankCard ;
+
     ParsiEditText editText ;
+    ParsiTextView textView ;
     ParsiButton btnChange ;
 
     @Override
@@ -22,15 +25,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.etPhoneNumber) ;
-        editText.addTextChangedListener(new BankCardTextWatcher(editText,8));
+        editText = findViewById(R.id.etCardNumber) ;
+        editText.addTextChangedListener(new BankCardTextWatcher(editText,16));
+        textView = findViewById(R.id.tvCardNumber) ;
+        textView.addTextChangedListener(new BankCardTextWatcher(textView,16));
+        textView.setText("6219861030604545");
         btnChange = findViewById(R.id.btnChange) ;
 
         btnChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                editText.setText("6219861030904261");
+                editText.setText(PidgetUtils.formatAmountWithCurrencyTagIR(String.valueOf(50000)));
             }
         });
     }
