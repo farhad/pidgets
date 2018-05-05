a5bf518a148035517fbbeec3403ff35579fef239
Farhad <ifarhadfaghihi@gmail.com>
2018-05-05 12:12:33 +0430

    fixed NPE in setHint

--- pidgets/src/main/java/io/github/farhad/widget/ParsiTextInputLayout.java
+++ pidgets/src/main/java/io/github/farhad/widget/ParsiTextInputLayout.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
+import android.text.TextUtils;
 import android.util.AttributeSet;
-
 import io.github.farhad.R;
 import io.github.farhad.typeface.FontType;
 import io.github.farhad.typeface.ParsiTypeface;
@@ -14,79 +14,84 @@ import io.github.farhad.utils.parsi.ParsiUtils;
 
 public class ParsiTextInputLayout extends TextInputLayout {
 
-    private boolean useParsiDigits ;
-    private FontType typefaceStyle ;
+  private boolean useParsiDigits;
+  private FontType typefaceStyle;
 
-    public ParsiTextInputLayout(Context context) {
-        super(context);
+  public ParsiTextInputLayout(Context context) {
+    super(context);
 
-        TypedArray typedArray = context.obtainStyledAttributes(R.styleable.ParsiTextInputLayout) ;
+    TypedArray typedArray = context.obtainStyledAttributes(R.styleable.ParsiTextInputLayout);
 
-        initialize(context, typedArray);
+    initialize(context, typedArray);
 
-        typedArray.recycle();
+    typedArray.recycle();
+  }
+
+  public ParsiTextInputLayout(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ParsiTextInputLayout);
+
+    initialize(context, typedArray);
+
+    typedArray.recycle();
+  }
+
+  public ParsiTextInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+
+    TypedArray typedArray = context
+        .obtainStyledAttributes(attrs, R.styleable.ParsiTextInputLayout, defStyleAttr,
+            defStyleAttr);
+
+    initialize(context, typedArray);
+
+    typedArray.recycle();
+  }
+
+  private void initialize(Context context, TypedArray typedArray) {
+    if (!isInEditMode()) {
+      useParsiDigits = typedArray.getBoolean(R.styleable.ParsiEditText_useParsiDigits, false);
+      typefaceStyle = FontType
+          .getType(typedArray.getInt(R.styleable.ParsiEditText_typefaceStyle, 0));
+
+      setTypeface(ParsiTypeface.getInstance().getMatchingTypeface(typefaceStyle));
     }
+  }
 
-    public ParsiTextInputLayout(Context context, AttributeSet attrs) {
-        super(context, attrs);
+  @Override
+  public void setHint(@Nullable CharSequence hint) {
 
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.ParsiTextInputLayout) ;
+    if (!TextUtils.isEmpty(hint)) {
 
-        initialize(context, typedArray);
+      String formattedHint = hint.toString();
 
-        typedArray.recycle();
+      if (useParsiDigits && PidgetUtils.containsDigits(formattedHint)) {
+        formattedHint = ParsiUtils.replaceWithParsiDigits(formattedHint);
+      }
+
+      super.setHint(formattedHint);
     }
+  }
 
-    public ParsiTextInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+  @Override
+  public void setTooltipText(@Nullable CharSequence tooltipText) {
+    super.setTooltipText(tooltipText);
+  }
 
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.ParsiTextInputLayout,defStyleAttr,defStyleAttr) ;
+  public boolean useParsiDigits() {
+    return useParsiDigits;
+  }
 
-        initialize(context, typedArray);
+  public void setUseParsiDigits(boolean useParsiDigits) {
+    this.useParsiDigits = useParsiDigits;
+  }
 
-        typedArray.recycle();
-    }
+  public FontType getTypefaceStyle() {
+    return typefaceStyle;
+  }
 
-    private void initialize(Context context, TypedArray typedArray)
-    {
-        if(!isInEditMode())
-        {
-            useParsiDigits = typedArray.getBoolean(R.styleable.ParsiEditText_useParsiDigits, false);
-            typefaceStyle  = FontType.getType(typedArray.getInt(R.styleable.ParsiEditText_typefaceStyle, 0));
-
-            setTypeface(ParsiTypeface.getInstance().getMatchingTypeface(typefaceStyle));
-        }
-    }
-
-    @Override
-    public void setHint(@Nullable CharSequence hint) {
-
-        String formattedHint = hint.toString() ;
-
-        if(useParsiDigits && PidgetUtils.containsDigits(formattedHint))
-            formattedHint = ParsiUtils.replaceWithParsiDigits(formattedHint) ;
-
-        super.setHint(formattedHint);
-    }
-
-    @Override
-    public void setTooltipText(@Nullable CharSequence tooltipText) {
-        super.setTooltipText(tooltipText);
-    }
-
-    public boolean useParsiDigits() {
-        return useParsiDigits;
-    }
-
-    public void setUseParsiDigits(boolean useParsiDigits) {
-        this.useParsiDigits = useParsiDigits;
-    }
-
-    public FontType getTypefaceStyle() {
-        return typefaceStyle;
-    }
-
-    public void setTypefaceStyle(FontType typefaceStyle) {
-        this.typefaceStyle = typefaceStyle;
-    }
+  public void setTypefaceStyle(FontType typefaceStyle) {
+    this.typefaceStyle = typefaceStyle;
+  }
 }
